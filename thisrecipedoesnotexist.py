4680abf553f41c66a878aba9d15c04906d4f0fde
Gcat101 <gcatmail2@gmail.com>
2024-03-27 17:44:49 +0300

    add comments to trdne

--- thisrecipedoesnotexist.py
+++ thisrecipedoesnotexist.py
@@ -16,44 +16,50 @@ LICENSE: WTFPL
   0. You just DO WHAT THE FUCK YOU WANT TO.
 """
 
+# IMPORTS
 from random import sample, choice, randrange
 from math import floor
 from glob import glob
 from os.path import exists
 from PIL import Image, ImageEnhance, ImageDraw
 from flask import Flask, request, send_file
-from typing import Coroutine
 from io import BytesIO
 from privatebinapi import send
 
+# Main run server function
 def run_server():
-    app = Flask(__name__)
+    app = Flask(__name__) # Create a flask app
 
     @app.get("/")
-    def recipe():
-        if "Discordbot" in request.user_agent.string: return ""
+    def recipe(): # Root path (nested functions wow)
+        if "Discordbot" in request.user_agent.string: return "" # Disable discord embeds by blocking useragents with "DiscordBot" in them
         
-        imgbin = BytesIO()
-        img, links = create(request.args.get("type"), request.args.get("output"), False)
-        img.save(imgbin, "PNG")
-        imgbin.seek(0)
+        # Create the image
+        imgbin = BytesIO() # Make a temp png
+        img, _ = create(request.args.get("type"), request.args.get("output"), False) # Generate the image with provided args
+        img.save(imgbin, "PNG") # Save it to the temp png io
+        imgbin.seek(0) # Write to memory
 
-        response = send_file(imgbin, "image/png")
+        # Response logic
+        response = send_file(imgbin, "image/png") # Initiate the response with "image/png" type
+        # Disable cache
         response.headers["Cache-Control"] = "no-store, must-revalidate"
         response.headers['Cache-Control'] = 'public, max-age=0'
-        return response
+        return response # Send the response
 
-    app.run(port=3333)
+    app.run(port=3333) # Run flask app on 3333
 
-def _get_item(path:str) -> str: return ":".join(path.split("/")[-2:]).removesuffix(".png")
-def get_path(itemid:str) -> str: 
+# HELPER FUNCTIONS
+def _get_item(path:str) -> str: return ":".join(path.split("/")[-2:]).removesuffix(".png") # Get the item name from the png filename
+def get_path(itemid:str) -> str: # Get the path from the item id
     path = f"assets/thisrecipedoesnotexist/items/{itemid.replace(':', '/')}.png"
     if not exists(path): return None
     return path
 
+# Generate a kjs script that adds the imaginary recipe
 def _generate_kjs_script(items: list[str], type: str) -> str:
-    output = items.pop()
-    tiernums = {
+    output = items.pop() # Get the last item as output, leaving only inputs in the items argument
+    tiernums = { # Map crafting table sizes to their tier numbers
         "3x3": 1,
         "5x5": 2,
         "7x7": 3,
@@ -78,16 +84,18 @@ event.recipes.extendedcrafting.shapeless_table("{output}", [
 ]).merge({{tier: {tiernums[type]}}})
 """
 
+# MAIN FUNCTION
 def create(type:str, outputitem:str, generatepastes:bool) -> (Image.Image, tuple[str]):
-    PARAMS = {
+    PARAMS = { # Set different parameters based on the crafting table size requested
         "3x3": ((122, 68), (318, 38), 60, 12, 3, "basic_table"),
         "5x5": ((58, 73), (270, 57), 60, 12, 5, "advanced_table"),
         "7x7": ((32, 72), (256, 63), 62, 10, 7, "elite_table"),
         "9x9": ((32, 72), (248, 63), 64, 8, 9, "ultimate_table")
     }
 
-    if type==None or type not in PARAMS.keys(): type=choice(list(PARAMS.keys()))
+    if type==None or type not in PARAMS.keys(): type=choice(list(PARAMS.keys())) # Randomly select a crafting table if not provided
     
+    # Convert random ass data from tuples into readable variables
     selectedparams = PARAMS[type]
     offset = selectedparams[0]
     outputoffset = selectedparams[1]
@@ -96,40 +104,43 @@ def create(type:str, outputitem:str, generatepastes:bool) -> (Image.Image, tuple
     benchsize = selectedparams[4]
     name = selectedparams[5]
 
-    files = glob("assets/thisrecipedoesnotexist/items/**/*.png")
-    if outputitem!=None:
+    files = glob("assets/thisrecipedoesnotexist/items/**/*.png") # Get all texture files from assets/thisrecipedoesnotexist/items
+    if outputitem!=None: # If the output item id was provided, convert it to the texture file path and make it so it can't be an input
         if ":" not in outputitem: outputitem = "minecraft:"+outputitem
         outputitem = get_path(outputitem)
         if outputitem!=None: files.remove(outputitem)
-    inputs = []
-    randitems = sample(files, k=(benchsize**2)+(1 if outputitem==None else 0))
-    if outputitem!=None: randitems.append(outputitem)
+    randitems = sample(files, k=(benchsize**2)+(1 if outputitem==None else 0)) # Randomly pick input items and 1 output item
+    if outputitem!=None: randitems.append(outputitem) # Add the output item to the randitems list if specified
 
+    # Iterate over all of the randomly selected items
+    inputs = []
     for i in randitems:
-        img : Image.Image = Image.open(i)
-        if img.height!=img.width: 
+        img : Image.Image = Image.open(i) # Open the texture image
+        if img.height!=img.width: # Make the image a square so it wouldn't stretch
             randpart = randrange(1, floor(img.height/img.width))
             img = img.crop((0, img.width*randpart, img.width, img.width*(randpart+1)))
-        inputs.append(img.resize((itemsize, itemsize), Image.NEAREST).convert("RGBA"))
-    output = inputs.pop()
+        inputs.append(img.resize((itemsize, itemsize), Image.NEAREST).convert("RGBA")) # Add the image object to inputs list
+    output = inputs.pop() # Pop the last input and make it an output
 
-    cell = itemsize*benchsize
-    grid : Image.Image = Image.open(f"assets/thisrecipedoesnotexist/gui/{name}.png")
-    grid = grid.resize((grid.width*4, grid.height*4), Image.NEAREST)
-    for i, img in enumerate(inputs): grid.paste(img, (offset[0]+(itemsize+gap)*(i%benchsize), offset[1]+(itemsize+gap)*(i//benchsize)), img if img.mode=="RGBA" else None)
-    grid.paste(output, (cell+outputoffset[0], (offset[1]+cell+outputoffset[1])//2), output if output.mode=="RGBA" else None)
+    cell = itemsize*benchsize # Find the place where the output should be pasted
+    grid : Image.Image = Image.open(f"assets/thisrecipedoesnotexist/gui/{name}.png") # Open the crafting bench GUI image
+    grid = grid.resize((grid.width*4, grid.height*4), Image.NEAREST) # Resize it to be a little big bigger with nearest neighbor
+    for i, img in enumerate(inputs): grid.paste(img, (offset[0]+(itemsize+gap)*(i%benchsize), offset[1]+(itemsize+gap)*(i//benchsize)), img if img.mode=="RGBA" else None) # Place the inputs
+    grid.paste(output, (cell+outputoffset[0], (offset[1]+cell+outputoffset[1])//2), output if output.mode=="RGBA" else None) # Place the output
 
-    bg : Image.Image = Image.open(choice(glob("assets/thisrecipedoesnotexist/bg/*.png")))
-    bg = ImageEnhance.Brightness(bg).enhance(.3)
-    grid.thumbnail((grid.width/1.5, grid.height), Image.Resampling.LANCZOS)
+    bg : Image.Image = Image.open(choice(glob("assets/thisrecipedoesnotexist/bg/*.png"))) # Open a random background image
+    bg = ImageEnhance.Brightness(bg).enhance(.3) # Reduce the brightness to make it look realistic
+    grid.thumbnail((grid.width/1.5, grid.height), Image.Resampling.LANCZOS) # Resize the GUI image
 
-    pastepoint = (bg.width//2-grid.width//2, bg.height//2-grid.height//2)
-    bg.paste(grid, pastepoint, grid)
+    pastepoint = (bg.width//2-grid.width//2, bg.height//2-grid.height//2) # Get the center of the background image
+    bg.paste(grid, pastepoint, grid) # Paste the GUI atop of the background image
 
+    # Randomly select an offset so the "screenshot" looks more realistic
     minoffset = -25
     maxoffset = 100
     bg = bg.crop((pastepoint[0]-randrange(minoffset, maxoffset), pastepoint[1]-randrange(minoffset, maxoffset), pastepoint[0]+grid.width+randrange(minoffset, maxoffset), pastepoint[1]+grid.height))
 
+    # Add watermark
     watermark = Image.new("RGBA", (bg.width, bg.height), "#00000000")
     draw = ImageDraw.Draw(watermark)
     draw.rectangle(((bg.width-30, 0), (bg.width-15, 15)), "#f80cf966")
@@ -138,9 +149,12 @@ def create(type:str, outputitem:str, generatepastes:bool) -> (Image.Image, tuple
     draw.rectangle(((bg.width-30, 15), (bg.width-15, 30)), "#00000066")
     bg = Image.alpha_composite(bg, watermark)
 
+    # Generate a kjs script and post it to privatebin if requested
     kjslink = None
     if generatepastes: kjslink = send("https://privatebin.net/", text=_generate_kjs_script([_get_item(i) for i in randitems], type))["full_url"]
 
-    return bg, ((kjslink, "") if generatepastes else None)
+    return bg, ((kjslink, "") if generatepastes else None) # Return the image and links (if requested)
 
-if __name__=="__main__": run_server()
+if __name__=="__main__": run_server() # Run the flask server if the file is not imported
+
+# ok that's it ty for your attention
